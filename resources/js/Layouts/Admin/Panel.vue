8d76befed7b1af5d41830a6c5c48d00db6c083f0
Taras Kulyk <taras.kulyk1@gmail.com>
2022-07-05 15:55:24 +0300

    Try to use vite

--- resources/js/Layouts/Admin/Panel.vue
+++ resources/js/Layouts/Admin/Panel.vue
@@ -1,41 +1,31 @@
 
 <template>
-  <div
-    v-show="this.$store.state.theme.layout === 'landing'"
-    class="landing-bg h-100 bg-gradient-primary position-fixed w-100"
-  ></div>
-  <sidenav
-    :class="[
-      this.$store.state.theme.isTransparent,
-      this.$store.state.theme.isRTL ? 'fixed-end' : 'fixed-start'
-    ]"
-    v-if="this.$store.state.theme.showSidenav"
-  />
-  <main
-    class="main-content position-relative max-height-vh-100 h-100 border-radius-lg"
-  >
+
+
+
+
+
+
+  <div v-show="this.$store.state.theme.layout === 'landing'" class="landing-bg h-100 bg-gradient-primary position-fixed w-100"></div>
+  <sidenav :class="[
+    this.$store.state.theme.isTransparent,
+    this.$store.state.theme.isRTL ? 'fixed-end' : 'fixed-start'
+  ]" v-if="this.$store.state.theme.showSidenav" />
+  <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
     <!-- nav -->
-    <navbar
-      :class="[navClasses]"
-      :textWhite="
-        this.$store.state.theme.isAbsolute ? 'text-white opacity-8' : 'text-white'
-      "
-      :minNav="navbarMinimize"
-      v-if="this.$store.state.theme.showNavbar"
-    />
-    <slot/>
+    <navbar :class="[navClasses]" :textWhite="
+      this.$store.state.theme.isAbsolute ? 'text-white opacity-8' : 'text-white'
+    " :minNav="navbarMinimize" v-if="this.$store.state.theme.showNavbar" />
+    <slot />
     <app-footer v-show="this.$store.state.theme.showFooter" />
-    <configurator
-      :toggle="toggleConfigurator"
-      :class="[
-        this.$store.state.theme.showConfig ? 'show' : '',
-        this.$store.state.theme.hideConfigButton ? 'd-none' : ''
-      ]"
-    />
+    <configurator :toggle="toggleConfigurator" :class="[
+      this.$store.state.theme.showConfig ? 'show' : '',
+      this.$store.state.theme.hideConfigButton ? 'd-none' : ''
+    ]" />
   </main>
 </template>
 <script>
-import Sidenav from "@/Components/Admin/Sidenav";
+import Sidenav from "@/Components/Admin/Sidenav.vue";
 import Configurator from "@/Components/Admin/Configurator.vue";
 import Navbar from "@/Components/Admin/Navbars/Navbar.vue";
 import NavbarNotification from "@/Components/Admin/Navbars/NavbarNotification.vue";
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,13 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import store from './store'
 import Panel from './Layouts/Admin/Panel.vue'
 
 createInertiaApp({
   resolve: name => {
-    const page = require(`./Pages/${name}`).default;
+    // const page = require(`./Pages/${name}`).default;
+    const page = resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')).default;
     if (page.layout === undefined) {
       page.layout = Panel
     }
